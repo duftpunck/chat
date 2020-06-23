15f8068379234f000a106d1d97b59ab9aae63a6c
duftpunck <duftpunck@gmail.com>
2020-06-23 17:58:31 +0300

    Add chat models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :chat_rooms, dependent: :destroy
+  has_many :messages, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  def name
+    email.split('@')[0]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-
-  root to: "home#index"
+  root to: "home#index"  
+  resources :chat_rooms, only: [:new, :create, :show, :index]
+  root 'chat_rooms#index'
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
